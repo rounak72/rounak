3123993cdc14e6d88948cda06908c6c3afdeddde
rounak72 <54244388+rounak72@users.noreply.github.com>
2020-09-29 23:02:15 +0530

    Updates

---
+++ precache-manifest.1f907c6173bda47eb89c8946628d876b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "603c0bbe0f78e350700fe93a47d6a71b",
+    "url": "/Robot/index.html"
+  },
+  {
+    "revision": "6a586f54a6b048ea630f",
+    "url": "/Robot/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5fd8655071802f99771a",
+    "url": "/Robot/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a586f54a6b048ea630f",
+    "url": "/Robot/static/js/2.571595fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robot/static/js/2.571595fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fd8655071802f99771a",
+    "url": "/Robot/static/js/main.74bab992.chunk.js"
+  },
+  {
+    "revision": "dad79fc238788c11e770",
+    "url": "/Robot/static/js/runtime-main.3d244804.js"
+  }
+]);
